461e9fbfaa725c9e30d2bdab413efbc9af43144a
a.harlamov <a.harlamov@profiscope.io>
2024-09-07 20:08:38 +0300

    stats docs

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # demand-hardware-failure
+
+## Внутреннее API
+
+### Модуль stats
+
+#### `iter_file(filepath: str) -> Generator[tuple[datetime, str, str, bool], None, None]`
+
+Генерирует строки из CSV-файла
+
+#### `iter_files(data_path: str) -> Generator[tuple[datetime, str, str, bool], None, None]`
+
+Генерирует строки из папки с CSV-файлов
+
+#### `get_data(folder: str = 'data') -> Generator[FailureInfo, None, None]`
+
+Генерирует модели `FailureInfo()` по всем CSV-файлам в папке `folder` 
+
+```python
+@dataclass
+class FailureInfo:
+    serial_number: str
+    model: str
+    start_date: datetime
+    failure_date: datetime
+```
+
+#### `init_sqlite3(conn)`
+
+Инициализация SQLite3 базы данных и таблицы `failure_info`, содержащая четыре колонки:
+
+- `serial_number` - серийный номер
+- `model` - модель
+- `start_date` - дата начала работы
+- `failure_date` - дата отказа
+
+#### `fill_data(conn, data_generator: Iterable[FailureInfo]) -> None`
+
+Заполняет таблицу `failure_info` соответствующими сущностями
+
+Используется как обучение или дообучение
+
+#### `get_statistics_by_models(conn, percentile: float = 0.9) -> list[tuple[str, float]]`
+
+Получает список перцентилей дней отказов по каждой модели, опираясь на таблицу `failure_info`
+
+Используется как предсказание
--- stats.py
+++ stats.py
@@ -9,11 +9,11 @@ from typing import Generator, Iterable
 class FailureInfo:
     serial_number: str
     model: str
-    start_date: str
-    failure_date: str
+    start_date: datetime
+    failure_date: datetime
 
 
-def iter_file(filepath: str) -> Generator[dict, None, None]:
+def iter_file(filepath: str) -> Generator[tuple[datetime, str, str, bool], None, None]:
     with open(filepath) as file:
         date = datetime.strptime(filepath.split('/')[1].split('.csv')[0], '%Y-%m-%d')
         for line in file:
@@ -21,12 +21,12 @@ def iter_file(filepath: str) -> Generator[dict, None, None]:
             yield date, items[1], items[2], items[4] == '1'
 
 
-def iter_files(data_path: str) -> Generator[tuple[str, str, str, bool], None, None]:
+def iter_files(data_path: str) -> Generator[tuple[datetime, str, str, bool], None, None]:
     for filepath in sorted(os.listdir(data_path)):
         yield from iter_file(os.path.join(data_path, filepath))
 
 
-def get_data(folder='data') -> Generator[FailureInfo, None, None]:
+def get_data(folder: str = 'data') -> Generator[FailureInfo, None, None]:
     first_log = {}
     for date, serial, model, failure in iter_files(folder):
         key = serial
@@ -71,7 +71,35 @@ def fill_data(conn, data_generator: Iterable[FailureInfo]) -> None:
         ))
 
 
+def get_statistics_by_models(
+    conn,
+    percentile: float = 0.9,
+) -> list[tuple[str, float]]:
+    cursor = conn.cursor()
+    cursor.execute('''
+    WITH data AS (
+        SELECT
+            model,
+            julianday(failure_date) - julianday(start_date) as diff
+        FROM failure_info
+    ),
+        ranked AS (
+            SELECT model,
+                   diff,
+                   row_number() over (ORDER BY diff) AS row_num,
+                   COUNT(*) OVER () AS total_rows
+            FROM data
+        )
+    SELECT model, AVG(diff) AS percentile
+    FROM ranked
+    WHERE row_num <= total_rows * ?
+    GROUP BY model;
+    ''', (percentile,))
+    return cursor.fetchall()
+
+
 if __name__ == '__main__':
     with sqlite3.connect(DATABASE_NAME) as conn:
         init_sqlite3(conn)
         fill_data(conn, get_data())
+        print(*get_statistics_by_models(conn, 0.9), sep='\n')
